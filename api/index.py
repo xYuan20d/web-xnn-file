40b06fe92bf98c0fbfa6e1db715a45c154c5440a
xiaoyy20d <lvzhiyuan0925@outlook.com>
2025-09-14 23:46:08 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,8 @@
 from flask import Flask
+from os import getcwd
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+    return getcwd()
