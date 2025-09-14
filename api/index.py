ed7272ce0e6a28310c31825975154a0d6413ce29
xiaoyy20d <lvzhiyuan0925@outlook.com>
2025-09-14 23:51:09 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,23 @@
-from flask import Flask
-from os import getcwd, listdir
+from flask import Flask, send_from_directory, abort
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return f"{getcwd()}, {listdir()}"
+# 设置你想要的文件目录 (可以是当前目录或者其他目录)
+BASE_DIRECTORY = os.getcwd()
+
+@app.route('/<path:filename>', methods=['GET'])
+def serve_file(filename):
+    # 构建文件路径
+    file_path = os.path.join(BASE_DIRECTORY, filename)
+
+    # 检查文件是否存在
+    if os.path.isfile(file_path):
+        # 通过 send_from_directory 返回文件
+        return send_from_directory(BASE_DIRECTORY, filename)
+    else:
+        # 如果文件不存在，返回 404
+        abort(404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
